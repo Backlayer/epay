98a8d61910246fec6ad432184d009b0eeff76716
Jerson Gil <jersongil21@gmail.com>
2022-12-28 22:16:44 -0400

    ⚡️  delete filters in transactions

--- script/resources/views/user/transactions/index.blade.php
+++ script/resources/views/user/transactions/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row mb-5">
-        <div class="col text-center">
+        <div class="col text-right">
             <a href="{{ route('user.transactions.index') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions')])>
                 <i class="fas fa-link"></i>
                 {{ __("All Transactions") }}
@@ -13,29 +13,13 @@
                 <i class="fas fa-link"></i>
                 {{ __("Single Charge") }}
             </a>
-            <a href="{{ route('user.transactions.index', 'donation') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/donation')])>
-                <i class="fas fa-gift"></i>
-                {{ __("Donation") }}
-            </a>
-            <a href="{{ route('user.transactions.index', 'qr-code') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/qr-code')])>
-                <i class="fas fa-arrow-down"></i>
-                {{ __("Qr Code") }}
-            </a>
             <a href="{{ route('user.transactions.index', 'invoice') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/invoice')])>
                 <i class="fas fa-envelope"></i>
                 {{ __("Invoice") }}
             </a>
-            <a href="{{ route('user.transactions.index', 'deposit') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/deposit')])>
-                <i class="fas fa-arrow-up"></i>
-                {{ __("Deposit") }}
-            </a>
             <a href="{{ route('user.transactions.index', 'website') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/website')])>
                 <i class="fas fa-laptop"></i>
                 {{ __("Website") }}
-            </a>
-            <a href="{{ route('user.transactions.index', 'plan') }}" @class(["latter-space-0 nav-link d-inline-block", "active" => Request::is('user/transactions/plan')])>
-                <i class="fas fa-user"></i>
-                {{ __("Your Subscriptions") }}
             </a>
         </div>
     </div>
