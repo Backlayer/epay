6d58802c1fb44c192ede4f475875ed6281c21d79
Carlos Camacho <carloscgo123@gmail.com>
2023-01-30 08:24:50 -0500

    refactor: method
    user dashboard

--- script/app/Http/Controllers/User/DashboardController.php
+++ script/app/Http/Controllers/User/DashboardController.php
@@ -35,8 +35,10 @@ class DashboardController extends Controller
 
     function transactions(Request $request)
     {
+        $year = $request->year ?? date("Y");
+
         $months = Transaction::whereUserId(auth()->id())
-            ->whereYear('created_at', $request->year ?? date("Y"))
+            ->whereYear('created_at', $year)
             ->selectRaw('month(created_at) month')
             ->groupBy('month')
             ->get()
@@ -45,8 +47,9 @@ class DashboardController extends Controller
                 return $data;
             });
 
-        $credit = Transaction::whereUserId(auth()->id())->where('amount', '>', 0)
-            ->whereYear('created_at', $request->year ?? date("Y"))
+        $credit = Transaction::whereUserId(auth()->id())
+            ->where('amount', '>', 0)
+            ->whereYear('created_at', $year)
             ->selectRaw('month(created_at) month, sum(amount) amount')
             ->groupBy('month')
             ->get()
@@ -56,8 +59,9 @@ class DashboardController extends Controller
                 return $data;
             });
 
-        $debit = Transaction::whereUserId(auth()->id())->where('amount', '<', 0)
-            ->whereYear('created_at', $request->year ?? date("Y"))
+        $debit = Transaction::whereUserId(auth()->id())
+            ->where('amount', '<', 0)
+            ->whereYear('created_at', $year)
             ->selectRaw('month(created_at) month, sum(amount) amount')
             ->groupBy('month')
             ->get()
