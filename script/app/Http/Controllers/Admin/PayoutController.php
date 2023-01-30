e10afff590fc3fe22caae6703b302c660b7c8bfe
Carlos Camacho <carloscgo123@gmail.com>
2023-01-30 12:09:59 -0500

    feat: payouts
    add to transactions history

--- script/app/Http/Controllers/Admin/PayoutController.php
+++ script/app/Http/Controllers/Admin/PayoutController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\User;
-use App\Models\Payout;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Payout;
+use App\Models\Transaction;
 use App\Mail\PayoutMail;
-use Illuminate\Support\Facades\Mail;
 
 class PayoutController extends Controller
 {
@@ -43,25 +44,76 @@ class PayoutController extends Controller
         return view('admin.payouts.show', compact('payout'));
     }
 
+    private function approvePending($user) {
+        $withdraws = Payout::where('user_id', $user->id)->get();
+
+        foreach ($withdraws as $withdraw) {
+            if ($withdraw->status == 'approved') {
+                if ($user->wallet >= $withdraw->amount) {
+                    $user->update([
+                        'wallet' => $user->wallet - $withdraw->amount
+                    ]);
+
+                    $name = $user->business_name ?? $user->name;
+
+                    Transaction::create([
+                        'name' => $name,
+                        'email' => $user->email,
+                        'user_id' => $user->id,
+                        'currency_id' => $user->currency_id,
+                        'amount' => -$withdraw->amount,
+                        'charge' => $withdraw->charge,
+                        'rate' => $user->currency->rate,
+                        'reason' => __('Payout sent to :name', ['name' => $name]),
+                        'type' => 'debit'
+                    ]);
+                }
+            }
+        }
+    }
+
     public function approved(Request $request)
     {
         $withdraw = Payout::find($request->withdraw);
-        if ($withdraw->status == 'rejected') {
+
+        if ($withdraw->status == 'pending') {
             $user = User::find($withdraw->user_id);
+
             if ($user->wallet >= $withdraw->amount) {
                 $user->update([
                     'wallet' => $user->wallet - $withdraw->amount
                 ]);
+
+                $name = $user->business_name ?? $user->name;
+
+                Transaction::create([
+                    'name' => $name,
+                    'email' => $user->email,
+                    'user_id' => $user->id,
+                    'currency_id' => $user->currency_id,
+                    'amount' => -$withdraw->amount,
+                    'charge' => $withdraw->charge,
+                    'rate' => $user->currency->rate,
+                    'reason' => __('Payout sent to :name', ['name' => $name]),
+                    'type' => 'debit'
+                ]);
             } else {
-                return back()->with('error', __('Insufficient balance.'));
+                return response()->json([
+                    'message' => __('Insufficient balance.'),
+                    'redirect' => route('admin.payouts.index')
+                ]);
             }
+
+            $this->approvePending($user);
         }
+
         // Send Email to admin
         if (env('QUEUE_MAIL')) {
             Mail::to(env('MAIL_TO'))->queue(new PayoutMail($withdraw));
         } else {
             Mail::to(env('MAIL_TO'))->send(new PayoutMail($withdraw));
         }
+
         $withdraw->update([
             'status' => 'approved'
         ]);
@@ -74,11 +126,12 @@ class PayoutController extends Controller
 
     public function reject(Request $request)
     {
-        $withdraw = Payout::find($request->withdraw);
         $user = User::find($withdraw->user_id);
         $user->update([
             'wallet' => $user->wallet + $withdraw->amount
         ]);
+
+        $withdraw = Payout::find($request->withdraw);
         $withdraw->update([
             'status' => 'rejected'
         ]);
@@ -96,10 +149,12 @@ class PayoutController extends Controller
                 'message' => __("Please select at least one item")
             ], 422);
         }
+
         foreach ($request->ids as $id) {
             $withdraw = Payout::find($id);
             $withdraw->delete();
         }
+
         return response()->json([
             'message' => __('Payout deleted successfully'),
             'redirect' => route('admin.payouts.index')
@@ -112,6 +167,7 @@ class PayoutController extends Controller
         $data['approved'] = Payout::where('status', 'approved')->count();
         $data['rejected'] = Payout::where('status', 'rejected')->count();
         $data['pending'] = Payout::where('status', 'pending')->count();
+
         return response()->json($data);
     }
 }
